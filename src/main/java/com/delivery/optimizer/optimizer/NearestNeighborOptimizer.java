f0078a963a0debd6de743e5c36f804339f1c1599
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-11-05 15:52:31 +0100

    feat(liquibase): ajout des entités Customer et DeliveryHistory (V2)

--- src/main/java/com/delivery/optimizer/optimizer/NearestNeighborOptimizer.java
+++ src/main/java/com/delivery/optimizer/optimizer/NearestNeighborOptimizer.java
@@ -8,7 +8,15 @@ import com.delivery.optimizer.util.DistanceCalculator;
 import java.util.*;
 
 public class NearestNeighborOptimizer implements TourOptimizer{
-    private   DistanceCalculator  distanceCalculator;
+    private final DistanceCalculator distanceCalculator;
+
+    public NearestNeighborOptimizer() {
+        this(new DistanceCalculator());
+    }
+
+    public NearestNeighborOptimizer(DistanceCalculator distanceCalculator) {
+        this.distanceCalculator = distanceCalculator;
+    }
     @Override
     public List<Delivery> calculateOptimalTour(Warehouse warehouse, List<Delivery> deliveries) {
         List<Delivery> remaining = new ArrayList<>(deliveries);
