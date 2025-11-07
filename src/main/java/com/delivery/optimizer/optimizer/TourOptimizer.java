2cc8311c5dc62c5f645c33439faeb27a0689f7d6
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-11-07 16:05:03 +0100

    update interface TourOptimizer

--- src/main/java/com/delivery/optimizer/optimizer/TourOptimizer.java
+++ src/main/java/com/delivery/optimizer/optimizer/TourOptimizer.java
@@ -1,11 +1,12 @@
 package com.delivery.optimizer.optimizer;
 
 import com.delivery.optimizer.model.Delivery;
+import com.delivery.optimizer.model.OptimizedRoute;
 import com.delivery.optimizer.model.Warehouse;
 
 import java.util.List;
 
 public interface TourOptimizer {
 
-        List<Delivery> calculateOptimalTour(Warehouse warehouse, List<Delivery> deliveries);
+    OptimizedRoute optimize(Warehouse warehouse, List<Delivery> deliveries);
 }
