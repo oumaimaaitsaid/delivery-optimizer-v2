f2826febaba22fb7263af0199f6049afa7deb449
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-11-07 14:55:57 +0100

    ajouter DeliveryHistoryRepository

---
+++ src/main/java/com/delivery/optimizer/repository/DeliveryHistoryRepository.java
@@ -0,0 +1,18 @@
+package com.delivery.optimizer.repository;
+
+import com.delivery.optimizer.model.DeliveryHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.DayOfWeek;
+import java.util.List;
+
+@Repository
+// Spring ghadi ykriyi l'implémentation b'raso
+public interface DeliveryHistoryRepository extends JpaRepository<DeliveryHistory, Long>{
+
+
+    List<DeliveryHistory> findByDayOfWeek(DayOfWeek  dayOfWeek);
+
+    List<DeliveryHistory> findTop500ByOrderByDeliveryDateDesc();
+}
