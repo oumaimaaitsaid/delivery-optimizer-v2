b501f55d08b2f19879a8b0aa42b73e41df5485d1
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-11-12 15:06:30 +0100

    ajouter paginatio n

--- src/main/java/com/delivery/optimizer/repository/DeliveryRepository.java
+++ src/main/java/com/delivery/optimizer/repository/DeliveryRepository.java
@@ -2,8 +2,23 @@ package com.delivery.optimizer.repository;
 import org.springframework.stereotype.Repository;
 import com.delivery.optimizer.model.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.delivery.optimizer.model.DeliveryStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 @Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
 
+
+    List<Delivery> findByStatusOrderByPlannedTimeAsc(DeliveryStatus status);
+
+
+    @Query("SELECT d FROM Delivery d WHERE d.customer.id = :customerId")
+    Page<Delivery> findByCustomerIdWithPagination(
+            @Param("customerId") Long customerId,
+            Pageable pageable
+    );
 
 }
